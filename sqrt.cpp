d83eaf351a54ad14f3caf1d00df34bf7cd72615e
arpita <arpitakasaudhan2019@gmail.com>
2022-06-13 16:16:24 +0530

    missing k ele

---
+++ sqrt.cpp
@@ -0,0 +1,26 @@
+/** input =8
+output= 2
+input =9
+output =3
+*/
+
+
+int mySqrt(int x) {
+      if(x <= 1) return x;
+
+		int l = 1;
+		int r = x;
+
+		while(l <= r) {
+			int mid = l + (r -l)/2;
+
+			if(mid == x/mid) {
+				return mid;
+			} if(mid > x/mid) {
+				r = mid -1;
+			} else {
+				l = mid +1;
+			}
+		}
+		return r;  
+    }

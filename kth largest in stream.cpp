fd8c4fa37b2eaafd7190165003d67f798d7f9bd7
arpita <arpitakasaudhan2019@gmail.com>
2022-08-01 23:24:07 +0530

    kth largest in stream

---
+++ kth largest in stream.cpp
@@ -0,0 +1,32 @@
+/*
+Kth Largest Element in a Stream
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+*/
+
+
+ int in_k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    KthLargest(int k, vector<int>& nums) {
+        in_k = k;
+        for (auto num : nums){
+            minHeap.push(num);
+            if (minHeap.size() > k)
+                minHeap.pop();    
+        }
+        
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size() > in_k)
+            minHeap.pop();
+        
+        return minHeap.top();
+    }

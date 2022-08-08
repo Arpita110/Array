e6da1faa3f8265b65d49777da9e8adea5208b5d3
arpita <arpitakasaudhan2019@gmail.com>
2022-08-09 01:20:37 +0530

    flood fill

---
+++ floodfill.cpp
@@ -0,0 +1,36 @@
+/*
+FLOOD FILL
+image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+*/
+ vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+		int n = image[0].size();
+		
+		queue<pair<int,int>> q;
+		q.push(make_pair(sr,sc));
+		
+		long long int precolor = image[sr][sc];
+		if(precolor == color) return image;
+		
+		while(!q.empty()){
+			
+			int i = q.front().first;
+			int j = q.front().second;
+			image[i][j] = color;
+			q.pop();
+			
+			if(i-1 >= 0 && image[i-1][j] == precolor)
+				q.push(make_pair(i-1,j));
+			
+			if(i+1 < m && image[i+1][j] == precolor)
+				q.push(make_pair(i+1,j));
+			
+			if(j-1 >= 0 && image[i][j-1] == precolor)
+				q.push(make_pair(i,j-1));
+			
+			if(j+1 < n && image[i][j+1] == precolor)
+				q.push(make_pair(i,j+1)); 
+		}
+	   return image;
+    }
